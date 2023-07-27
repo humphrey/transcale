40bd37ced09f406c878c941c057f79e32c0d20e6
Humphrey Murray <humphreymurray@gmail.com>
2023-07-27 21:06:19 +1000

    Basic styling

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,12 @@ import { Scale } from './Scale';
 import { useScaleGestures } from './useScaleGestures';
 
 
+const defaultRange = {min: -50, max: 50};
+
+
 function App() {
 
-  const [range,  setRange] = React.useState({min: -50, max: 50})
+  const [range,  setRange] = React.useState(defaultRange)
   const celsiusToFahrenheit = (c: number) => (c * 9/5) + 32;
 
   const bind = useScaleGestures({
@@ -24,7 +27,7 @@ function App() {
   })
 
   return (
-    <div {...bind} style={{touchAction: 'none'}}>
+    <div {...bind} style={{touchAction: 'none', userSelect: 'none'}}>
       <Scale
         left={{
           range,
@@ -35,6 +38,12 @@ function App() {
           format: degrees => `${degrees}°F`,
         }}
       />
+      <div style={{position: 'fixed', top: 0, left: 0, right: 0, padding: '3px', background: '#035', textAlign: 'center', fontSize: '85%'}}>
+        Transcale: Your Visual Converter
+      </div>
+      <div style={{position: 'fixed', bottom: 0, left: 0, right: 0, padding: '3px', background: '#222', borderTop: '1px solid #444', color: '#777', textAlign: 'center', fontSize: '85%'}}>
+        Tip: Try dragging and pinching to zoom. <span onClick={() => setRange(defaultRange)}>[RESET]</span>
+      </div>
     </div>
   )
 }
